9c3b5b9d96b346820272f2fe8a98028827ee0991
Julien J. Simons <52903784+JulienSimons@users.noreply.github.com>
2024-01-23 13:48:04 -0500

    Making port environment for gateway connection.

---
+++ run.R
@@ -0,0 +1,19 @@
+#####################################
+## Julien J. Simons                ##
+## 12/08/23 - 12/19/23             ##
+## https://github.com/JulienSimons ##
+#####################################
+## Making port environment for gateway connection.
+
+library(shiny)
+
+## If there is no default port environment variable set on the local machine,
+## then use ShinyApps default port number to satisfy the gateway connection.
+port <- Sys.getenv('PORT')
+if (port == "") port <- 3838
+
+shiny::runApp(
+  appDir = getwd(),
+  host = '0.0.0.0',
+  port = as.numeric(port)
+)
