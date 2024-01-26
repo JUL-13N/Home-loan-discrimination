bf2f84b8c701d050a2345f93fa3dea1082288061
Julien J. Simons <52903784+JulienSimons@users.noreply.github.com>
2024-01-26 00:49:21 -0500

    Computing unique, non-numeric data for UI selectInput.

--- ui/data/dictionary/ui_data_dc_v01.R
+++ ui/data/dictionary/ui_data_dc_v01.R
@@ -3,9 +3,9 @@
 ## 12/08/23 - 01/25/24             ##
 ## https://github.com/JulienSimons ##
 #####################################
-## 01/25/24 ui_data_B_v01: Computing unique, non-numeric data for UI selectInput.
+## 01/25/24 ui_data_dc_v01: Computing unique, non-numeric data for UI selectInput.
 
-## The ui.R file will read the created ui_data-B.csv file to generate
+## The ui.R file will read the created ui_data_dc.csv file to generate
 ## unique, non-numeric values for the selection menus in order to
 ## collect the user input for the machine learning model prediction.
 
