9ff314811a198a84984d9c5c5c1ee87c98c23cbc
Julien J. Simons <52903784+JulienSimons@users.noreply.github.com>
2024-02-01 03:50:54 -0500

    Detecting if a user is on mobile device.
    This is a required file for the website: https://juliensimons.shinyapps.io/home-loan/

---
+++ www/mobileDetection.js
@@ -0,0 +1,5 @@
+// JavaScript function to detect mobile devices
+function isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
+  
